26afb3b8011203eaf86ade475fe96bfda633af87
Zoe Stafford <zoepolarsax@gmail.com>
2025-03-11 17:00:21 +0000

    Convince C# it can actually compile `TilesDataTest` on linux
    You must specify where the `RhinoCommon.dll` file is (so just copy it from windows)

--- TilesDataTest/UnitTest1.cs
+++ TilesDataTest/UnitTest1.cs
@@ -5,28 +5,29 @@ namespace Test
     using Rhino.Testing.Fixtures;
     using Rhino.Geometry;
     using TilesData;
+    using System.Collections;
 
     [RhinoTestFixture]
     public class JsonDeser
     {
         [Test]
         [TestCaseSource(nameof(BoundingVolumeJson))]
-        public void BoundingVolumeShouldParse(string json, BoundingVolume? expected)
+        public void BoundingVolumeShouldParse(string json, BoundingVolume expected)
         {
-            var result = JsonSerializer.Deserialize<BoundingVolume>(json);
+            var result = JsonSerializer.Deserialize<BoundingVolume>(json, Tileset.JsonSerializerOptions);
             Assert.IsNotNull(result);
-            if (expected is not null) Assert.AreEqual(result, expected);
+            if (expected is not null) Assert.AreEqual(expected, result);
         }
 
-        public static IEnumerable<TestCaseData> BoundingVolumeJson()
+        public static IEnumerable BoundingVolumeJson()
         {
             yield return new TestCaseData(
                 """{ "sphere": [0.1, 0.4, 0.2, 0.5] }""",
-                new BoundingVolume(new BoundingSphere(new Point3d(0.1, 0.4, 0.2), 0.5))
+                BoundingVolume.From(new BoundingSphere(new Point3d(0.1, 0.4, 0.2), 0.5))
             );
             yield return new TestCaseData(
                 """{ "region": [2.5, 2.504, -1.4, -1.398, 20, 300] }""",
-                new BoundingVolume(new BoundingRegion(2.5, 2.504, -1.4, -1.398, 20, 300))
+                BoundingVolume.From(new BoundingRegion(2.5, 2.504, -1.4, -1.398, 20, 300))
             );
         }
     }
