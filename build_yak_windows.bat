82c4ffc6cb496fda6352e7dc3e6ab5e754a80629
Patrick Guan <yangchen.guan@cs.ox.ac.uk>
2025-05-09 13:59:34 +0100

    updated plugin filename and version

--- build_yak_windows.bat
+++ build_yak_windows.bat
@@ -4,5 +4,5 @@ copy manifest.yml LoadTiles\bin\Release\net7.0
 pushd LoadTiles\bin\Release\net7.0\
 "C:\Program Files\Rhino 8\System\Yak.exe" build
 popd
-move LoadTiles\bin\Release\net7.0\cesiumion-1.0.0-rh8_0-any.yak .\cesiumion-1.0.0-rh8_0-windows.yak
+move LoadTiles\bin\Release\net7.0\sealion-1.0.1-rh8_0-any.yak .\sealion-1.0.1-rh8_0-windows.yak
 
