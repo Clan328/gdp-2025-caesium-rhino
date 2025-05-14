43836923ecf0c1f401e227c1fa1aae6bca592aac
Patrick Guan <yangchen.guan@cs.ox.ac.uk>
2025-05-14 18:23:33 +0100

    update version to 1.1.0

--- build_yak_macos.sh
+++ build_yak_macos.sh
@@ -5,5 +5,5 @@ cp manifest.yml LoadTiles/bin/Release/net7.0
 pushd LoadTiles/bin/Release/net7.0/
 "/Applications/Rhino 8.app/Contents/Resources/bin/yak" build
 popd
-mv LoadTiles/bin/Release/net7.0/sealion-1.0.1-rh8_0-any.yak ./sealion-1.0.1-rh8_0-mac.yak
+mv LoadTiles/bin/Release/net7.0/sealion-1.1.0-rh8_0-any.yak ./sealion-1.1.0-rh8_0-mac.yak
 
